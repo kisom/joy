6dba599706c4c22d9200d7c813090c66cb1b99e7
Kyle Isom <coder@kyleisom.net>
2013-04-01 00:13:56 -0600

    update license in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject joy "0.2.0-firehose"
   :description "Example code working through the Joy of Clojure"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "ISC"
+            :url "http://tyrfingr.is/licenses/LICENSE.ISC"}
   :main joy.core
   :dependencies [[org.clojure/clojure "1.4.0"]])
